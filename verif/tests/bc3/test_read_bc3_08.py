be41faa36b8e2e5b8af9054268822bb79cb2179a
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2023-10-24 10:03:37 +0200

    Add metadata

--- verif/tests/bc3/test_read_bc3_08.py
+++ verif/tests/bc3/test_read_bc3_08.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
-'''Trivial PyCost test.''' 
+'''Trivial PyCost test.'''
+
 from __future__ import division
 from __future__ import print_function
+
+__author__= "Luis C. Pérez Tato (LCPT)"
+__copyright__= "Copyright 2022, LCPT"
+__license__= "GPL"
+__version__= "3.0"
+__email__= "l.pereztato@ciccp.es"
 
 from pycost.structure import obra
 from pycost.structure import chapter
